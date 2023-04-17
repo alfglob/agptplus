c585ae8c2be8dec09e266ef85b345575529bc9e8
alfglob <130988838+alfglob@users.noreply.github.com>
2023-04-17 14:24:45 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,11 @@ import { ThemeProvider } from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { RouterProvider } from 'react-router-dom';
 
-import { router } from './router';
 import { store } from './store';
 
 import { theme } from './theme';
+import { ConfigurePromptPage } from './pages/ConfigurePromptPage';
 import './assets/style/main.scss';
 // import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -19,7 +18,7 @@ root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Provider store={store}>
-        <RouterProvider router={router} />
+		<ConfigurePromptPage />
       </Provider>
     </ThemeProvider>
   </React.StrictMode>,
