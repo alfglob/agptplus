fa13bfa00c799b8350db5384e9e4a356738d9f91
alfglob <130988838+alfglob@users.noreply.github.com>
2023-04-18 13:58:17 +0200

    Update openAIApi.ts

--- src/services/api/openAIApi.ts
+++ src/services/api/openAIApi.ts
@@ -2,8 +2,8 @@ import { axiosBaseQuery } from './base.query';
 
 import { openAIHttp } from '../openAI.http.interceptor';
 
-//const openAIURL = process.env.REACT_APP_OPEN_AI_URL;
-const openAIURL ='https://world43434.onrender.com/api/chatgpt/proxychat';
+// const openAIURL = process.env.REACT_APP_OPEN_AI_URL;
+const openAIURL = 'https://world43434.onrender.com/api/chatgpt/proxychat';
 export const openAIApi = {
   askOpenAI: (messages: any) =>
     axiosBaseQuery({
